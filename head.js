9bb79694373eeb44ba02282b8cc19d1fb21e66ff
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-18 12:27:12 -0400

    improved test cases

--- head.js
+++ head.js
@@ -12,8 +12,8 @@ const head = (arr) => {
 // TESTS
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1, 2, 3]), "wow!");
-assertEqual(head(["one", "two", "three"]), 1);
+assertEqual(head([1, 2, 3]), 3);
+assertEqual(head(["one", "two", "three"]), "two");
 assertEqual(head([1]), 1);
 assertEqual(head([]), undefined);
 assertEqual(head([]), "something");
