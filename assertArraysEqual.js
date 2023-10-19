f66b8b9625f9e4234f1e5e9b7675508418a0e808
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-19 10:24:11 -0400

    edits to assertArraysEqual to correctly use eqArrays in context

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,19 @@
 // A function that takes two arrays as arguments and compares if they are a perfect match
-const assertArraysEqual = (arr1, arr2) => {
+const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`💔 Assertion failed: ${arr1} !== ${arr2}`);
       return false;
     }
   }
-  console.log(`💅 Assertion passed: ${arr1} === ${arr2}`);
   return true;
+};
+
+// A function that prints the result of eqArrays to the console
+const assertArraysEqual = (arr1, arr2) => {
+  console.log(eqArrays(arr1, arr2) ? `💅 Assertion passed: ${arr1} === ${arr2}` : `💔 Assertion failed: ${arr1} !== ${arr2}`);
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
