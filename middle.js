ac63037f5a412ab892eaee456eaa3eabcc9966a0
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-19 16:05:35 -0400

    adding more test cases

--- middle.js
+++ middle.js
@@ -30,11 +30,14 @@ const middle = (arr) => {
   }
 };
 
+// Empty
+assertArraysEqual(middle([]), []);
+// Too few
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
 // Odd
+assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 // Even
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// Too few
-assertArraysEqual(middle([1, 2]), []);
-// Empty
-assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
