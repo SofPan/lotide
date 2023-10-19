9086a9faf00d10ee392a7865ffea154c2f894cbf
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-19 09:46:13 -0400

    eqArrays.js exercise

---
+++ eqArrays.js
@@ -0,0 +1,40 @@
+// This function compares two arguments and outputs if they are equal or not
+const assertEqual = (actual, expected) => {
+  console.log(actual === expected ? `💅 Assertion passed: ${actual} === ${expected}`
+    : `💔 Assertion failed: ${actual} !== ${expected}`);
+};
+/*
+  A function that takes two arrays as arguments and compares if they are a perfect match:
+    If both arrays are not the same length:
+      return false
+    
+    Loop over both arrays:
+      If current index of both arrays do not match:
+        return false
+    End Loop
+
+    Return true
+*/
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// TESTS
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
