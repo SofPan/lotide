e5bb8d4e8720730311b82e50b3877e397af7f622
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-19 10:53:31 -0400

    implement without.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,4 +19,3 @@ const assertArraysEqual = (arr1, arr2) => {
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // should fail
 assertArraysEqual([1, 2, 3], [1, 2]); // should fail
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // should fail
---
+++ without.js
@@ -0,0 +1,45 @@
+// A function that takes two arrays as arguments and compares if they are a perfect match
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// A function that prints the result of eqArrays to the console
+const assertArraysEqual = (arr1, arr2) => {
+  console.log(eqArrays(arr1, arr2) ? `💅 Assertion passed: ${arr1} === ${arr2}` : `💔 Assertion failed: ${arr1} !== ${arr2}`);
+};
+
+// A function that takes two arrays as arguments and removes the elements in the second array from the first array, then returns the newly made array
+const without = (arr1, arr2) => {
+  return arr1.filter((x) => {
+    return !arr2.includes(x);
+  });
+};
+
+// TESTS
+// remove the first index
+const first = without([1, 2, 3], [1]); // => [2, 3]
+// remove the last index
+const last = without([1, 2, "3"], ["3"]);// => [1, 2]
+// not all elements are present in the first array
+const someNotIncluded = without([1, 2, "3", 4, 5], [3, "5", 5]); // => [1, 2, "3", 4]
+// all but one element should be removed
+const allButOne = without([1, 2, 3, 4, 5], [1, 2, 3, 4]); // => [5]
+
+// Test the original array is not modified
+const originalArray = ["a", "b", "c"];
+without(originalArray, ["b"]);
+console.log("original array: ", originalArray); // => originalArray unmodified
+
+// Testing assertions
+assertArraysEqual(first, [2, 3]);
+assertArraysEqual(last, [1, 2]);
+assertArraysEqual(someNotIncluded, [1, 2, "3", 4]);
+assertArraysEqual(allButOne, [5]);
