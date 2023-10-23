a1dc91c0465fdd6592fb8ca15dda15de15bdb4fb
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-23 19:39:07 -0400

    added countOnly function, for...of loop to console log all items, and some test assertions

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+// This function compares two arguments and outputs if they are equal or not
+const assertEqual = (actual, expected) => {
+  console.log(actual === expected ? `💅 Assertion passed: ${actual} === ${expected}`
+    : `💔 Assertion failed: ${actual} !== ${expected}`);
+};
+
+/**
+  * A function that takes in a collection of items and returns a count for a specific subset of those items
+  * @param allItems an array of strings
+  * @param itemsToCount an object for a subset of items to be counted
+*/
+const countOnly = (allItems, itemsToCount) => {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+  }
+
+  return results;
+};
+
+// TEST
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
+
