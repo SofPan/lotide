8d0d18fbc9602154399744d9c2e6e5eb73e1b9da
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-24 11:37:49 -0400

    implement findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,17 @@
+// This function compares two arguments and outputs if they are equal or not
+const assertEqual = (actual, expected) => {
+  console.log(actual === expected ? `💅 Assertion passed: ${actual} === ${expected}`
+    : `💔 Assertion failed: ${actual} !== ${expected}`);
+};
+
+/**
+ * @function findKeyByValue takes in an object and a value and outputs the matching key
+ * @param compareObject an object to be iterated over
+ * @param compareValue a value to be compared against
+ * @returns the key associated with compareValue or undefined
+ */
+
+const findKeyByValue = (compareObject, compareValue) => {
+
+};
+
