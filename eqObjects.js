4c4e9fdfb4562c493235e026c9a59e85f668caf2
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-24 11:54:44 -0400

    implement eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,16 @@
+// This function compares two arguments and outputs if they are equal or not
+const assertEqual = (actual, expected) => {
+  console.log(actual === expected ? `💅 Assertion passed: ${actual} === ${expected}`
+    : `💔 Assertion failed: ${actual} !== ${expected}`);
+};
+
+/**
+ * @function eqObjects a function that compares two objects for equality
+ * @param obj1
+ * @param obj2
+ * @returns true or false
+ */
+
+const eqObjects = (obj1, obj2) => {
+
+};
