9164026459562c8faaa917a4dad7a474a8388a5e
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-24 10:40:53 -0400

    set up for loop to iterate over sentence in letterPositions function

--- letterPositions.js
+++ letterPositions.js
@@ -1,15 +1,33 @@
-// This function compares two arguments and outputs if they are equal or not
-const assertEqual = (actual, expected) => {
-  console.log(actual === expected ? `💅 Assertion passed: ${actual} === ${expected}`
-    : `💔 Assertion failed: ${actual} !== ${expected}`);
+// A function that takes two arrays as arguments and compares if they are a perfect match
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// A function that prints the result of eqArrays to the console
+const assertArraysEqual = (arr1, arr2) => {
+  console.log(eqArrays(arr1, arr2) ? `💅 Assertion passed: ${arr1} === ${arr2}` : `💔 Assertion failed: ${arr1} !== ${arr2}`);
 };
 
 /**
  * @function letterPositions
- * @param str a string
+ * @param sentence a string
  * @returns the indexed positions of each occurrence of each letter in the string
 */
 
-const letterPositions = (str) => {
-
+const letterPositions = (sentence) => {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    console.log(sentence[i]);
+  }
+  return results;
 };
+
+console.log(letterPositions("hello"));
