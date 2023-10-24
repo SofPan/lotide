9916a383fa698405fbfd93bb611e31b2cf115a27
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-24 11:47:40 -0400

    added for...in loop to iterate over object and match against given value. Added test assertions

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -12,6 +12,36 @@ const assertEqual = (actual, expected) => {
  */
 
 const findKeyByValue = (compareObject, compareValue) => {
-
+  for (const key in compareObject) {
+    if (compareObject[key] === compareValue) {
+      return key;
+    }
+  }
+  return undefined;
 };
 
+//TESTS
+
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  "comedy": "Brooklyn Nine-Nine",
+  "drama": "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+// empty object test
+const emptyObject = {};
+
+assertEqual(findKeyByValue(emptyObject, "anything"), undefined);
+
+// multiple matching values should stop at first match
+const multipleMatchingValues = {
+  notAMatch: "nope",
+  isAMatch: "yes",
+  alsoAMatch: "yes",
+  somethingElse: "nothing to see here"
+};
+
+assertEqual(findKeyByValue(multipleMatchingValues, "yes"), "isAMatch");
