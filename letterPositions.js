e320bd8dd85f83d2397f010eec278f80a414d9e4
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-24 09:47:48 -0400

    set up letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,15 @@
+// This function compares two arguments and outputs if they are equal or not
+const assertEqual = (actual, expected) => {
+  console.log(actual === expected ? `💅 Assertion passed: ${actual} === ${expected}`
+    : `💔 Assertion failed: ${actual} !== ${expected}`);
+};
+
+/**
+ * @function letterPositions
+ * @param str a string
+ * @returns the indexed positions of each occurrence of each letter in the string
+*/
+
+const letterPositions = (str) => {
+
+};
