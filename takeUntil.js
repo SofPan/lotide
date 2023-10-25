45811d78789ba17aa7b9050df56ced8cdda28f63
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-25 11:10:28 -0400

    added test data and loop for array items

--- takeUntil.js
+++ takeUntil.js
@@ -25,8 +25,20 @@ const assertArraysEqual = (arr1, arr2) => {
 
 const takeUntil = (array, callback) => {
   const result = [];
-
+  for (const item of array) {
+    console.log("item before :", item);
+    console.log("item after :", callback(item));
+  }
   return result;
 };
 
-console.log(takeUntil());
+// TESTS
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
