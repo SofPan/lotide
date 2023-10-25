0ae61d16d8a162507b22c5992001c2801a9aa86c
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-10-25 11:17:21 -0400

    added test assertions

--- takeUntil.js
+++ takeUntil.js
@@ -43,18 +43,18 @@ const takeUntil = (array, callback) => {
 // test numbers
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 console.log('---');
 
 // Test strings
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
 
 console.log('---');
 
 // Test callback condition is never met
 const data3 = [1, 2, 3, 4, 5];
 const results3 = takeUntil(data3, x => x < 0);
-console.log(results3);
+assertArraysEqual(results3, data3);
