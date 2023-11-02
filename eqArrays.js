1a47b96157d4243fb80d05c7da768c977aea6bea
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-02 15:13:46 -0400

    return false when items do not match instead of continuing loop

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,7 @@ const eqArrays = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     if (!Array.isArray(arr1[i])) {
       if (arr1[i] !== arr2[i]) {
-        result = false;
+        return false;
       } else {
         result = true;
       }
