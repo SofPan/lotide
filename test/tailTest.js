f5ec5f30b870e2294f5f01a3da4c73d293a664cb
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-06 17:05:54 -0500

    refactor tailTest.js to use M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,27 +1,31 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-
-
 
 const words = ["word", "wordy", "wordiest"];
 const empty = [];
 const oneElement = [1];
 
-// Test that the original array in unchanged
-tail(words);
-assertEqual(words.length, 3);
-
-// Test that the array is the correct length
-assertEqual(words.length - 1, tail(words).length);
-// Test that the first index of the tail array is correct
-assertEqual("wordy", tail(words)[0]);
-// Test another index of the tail array is correct
-assertEqual("wordiest", tail(words)[1]);
-// Test for an empty array when expected
-assertEqual(0, tail(empty).length);
-assertEqual(0, tail(oneElement).length);
-
-// Test that the wrong index was not pulled in
-assertEqual(tail(words)[0], "word");
-// Test that the array not too long
-assertEqual(tail(words).length, 3);
+describe("#tail", () => {
+  it("returns the correct array length", () => {
+    assert.equal(words.length - 1, tail(words).length);
+  });
+  it("does not alter the original array", () => {
+    tail(words);
+    assert.equal(words.length, 3);
+  });
+  it("returns 'wordy' as the first element in the new array", () => {
+    assert.equal("wordy", tail(words)[0]);
+  });
+  it("returns 'wordiest' as the second element of the new array", () => {
+    assert.equal("wordiest", tail(words)[1]);
+  });
+  it("returns an empty array when passed an empty array", () => {
+    assert.equal(0, tail(empty).length);
+  });
+  it("returns an empty array when there is only one element in an array to start", () => {
+    assert.equal(0, tail(oneElement).length);
+  });
+  it("returns a matching array of elements", () => {
+    assert.deepStrictEqual(["wordy", "wordiest"], tail(words));
+  });
+});
