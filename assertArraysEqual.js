bc11c14b7cff4d6e7c33d79151d5a0a821372460
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-06 16:23:07 -0500

    refactor assertArraysEqual and add test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -5,8 +5,4 @@ const assertArraysEqual = (arr1, arr2) => {
   console.log(eqArrays(arr1, arr2) ? `💅 Assertion passed: ${arr1} === ${arr2}` : `💔 Assertion failed: ${arr1} !== ${arr2}`);
 };
 
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // should fail
-// assertArraysEqual([1, 2, 3], [1, 2]); // should fail
-
 module.exports = assertArraysEqual;
