c7fe9a48b6bb3f8e8d216d1e05dce5747cb6fe22
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-06 17:12:48 -0500

    refactor middleTest.js to use M&C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,26 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// Empty
-assertArraysEqual(middle([]), []);
-// Too few
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-// Odd
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// Even
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns [2] when passed [1, 2, 3]", () => {
+    assert.deepStrictEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3] when passed [1, 2, 3, 4, 5]", () => {
+    assert.deepStrictEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [2, 3] when passed [1, 2, 3, 4]", () => {
+    assert.deepStrictEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3, 4] when passed [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepStrictEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns [] when passed []", () => {
+    assert.deepStrictEqual(middle([]), []);
+  });
+  it("returns [] when passed [1]", () => {
+    assert.deepStrictEqual(middle([1]), []);
+  });
+  it("returns [] when passed [1, 2]", () => {
+    assert.deepStrictEqual(middle([1, 2]), []);
+  });
+});
