1a0520a5a064d3753d53fdb1cb21d3342da24537
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-06 19:15:28 -0500

    refactored eqArraysTest to use M&C

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,14 @@
+const assert = require('chai').assert;
 const eqArrays = require('../index').eqArrays;
 
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+describe("#eqArrays", () => {
+  it("returns true when nested arrays match perfectly", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it("returns false when nested arrays do not match", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+  it("returns false when arrays are mixed with numbers", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+});
